ec899aca21b459e94894aa1066c5048ca76c09de
Rene Roscher <r.roscher@r-services.eu>
2021-03-23 06:14:06 +0100

    initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,70 @@
+// https://raw.githubusercontent.com/LIVCK/livck-docs/main/README.md
+
+module.exports = {
+    title: 'LIVCK - Documentation',
+    description: 'Here can you find detailed documentation of the software.',
+    dest: './dist',
+
+    async additionalPages() {
+        const rp = require('request-promise');
+
+        const readme = await rp('https://raw.githubusercontent.com/LIVCK/livck-docs/main/README.md')
+        const changelog = await rp('https://raw.githubusercontent.com/LIVCK/livck-docs/main/changelog.md')
+        return [
+            {
+                path: '/',
+                content: readme
+            },
+            {
+                path: '/changelog/',
+                content: changelog
+            }
+        ]
+    },
+
+    themeConfig: {
+        sidebarDepth: 1,
+        search: false,
+        nav: [
+            {text: 'LIVCK', link: 'https://livck.com'},
+            {text: 'Github', link: 'https://github.com/LIVCK'},
+            {text: 'Twitter', link: 'https://twitter.com/LIVCKCOM'},
+        ],
+        sidebar: [
+
+
+            {
+                title: 'Getting Started',
+                collapsable: false,
+                children: [
+                    ['/', 'Installation & Usage'],
+                ],
+            },
+            {
+                title: 'Changelog',
+                collapsable: false,
+                children: [
+                    ['/changelog/', 'Changelog'],
+                ],
+            },
+
+        ],
+    },
+    head: [
+        ['link', {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png'}],
+        ['link', {rel: 'icon" type="image/png', sizes: '32x32', href: '/favicon-32x32.png'}],
+        ['link', {rel: 'icon" type="image/png', sizes: '16x16', href: '/favicon-16x16.png'}],
+        ['link', {rel: 'manifest', href: '/site.webmanifest'}],
+        ['style', {}, 'img + .icon.outbound {display: none;}'],
+    ],
+    plugins: [
+        [
+            'sitemap',
+            {
+                hostname: 'https://help.livck.com',
+            },
+        ],
+    ],
+}
+
+
---
+++ docs/changelog/index.md
@@ -0,0 +1,37 @@
+## 14. Settings
+#### The following setting values with their property.
+
+**Title:** <br>This value is displayed in the page title.
+<br><br>
+**Description:** <br>This value is used for Search engine optimization.
+<br><br>
+**Monitor Refresh Interval:** <br>This value determines in what second intervals the page is updated.
+<br><br>
+**Header Logo:** <br>This value must be a URL, but if you don't want an image on your page, you can leave it blank
+<br><br>
+**Header Title:** <br>This value is the header title, this is only displayed if the header logo is empty.
+<br><br>
+**Header Color:**<br> This value defines the following backgrounds:
+- Header
+- Alerts
+
+**Body Color:** <br>This value defines the body background.<br><br>
+**Element Color:** <br>This value defines the following backgrounds:
+- Tables
+- Inputs
+- Cards
+
+**Text Color:** <br>This value defines all of the text colors on the page.
+<br><br>
+**[Open Graph](https://ogp.me/):**<br> All values that begin with og: belong to the Open Graph Protocol. <br><br>
+**Whats is Open Graph?** <br>The Open Graph protocol enables any web page to become a rich object in a social graph. For instance, this is used on Facebook to allow any web page to have the same functionality as any other object on Facebook.
+<br><br>
+**Twitter:** <br>All values starting with twitter: belong to the Twitter-Cards, these let a post with a link from your site look more informative.
+<br><br>
+**Keywords:** <br>The keywords are to be given as a list, e.g. "livck, status"
+<br><br>
+**Robots:** <br>The robots are to be given as a list, e.g. "index, follow"
+<br><br>
+**Date-Format:** <br>The date format is made up of letters, each of which stands for a value in the date. [To build a valid format](https://www.w3.org/TR/NOTE-datetime).
+<br><br>
+**Monitor Timeout:** <br>This value is given in seconds and determines how long to wait on a monitor if it takes longer to respond.
